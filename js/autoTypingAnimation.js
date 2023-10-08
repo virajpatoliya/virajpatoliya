aaf755f2d1fdbf61873d9b6abfefcb56f6281be6
virajpatoliya <virajpatoliya44@gmail.com>
2023-10-09 05:07:07 +0530

    first

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Viraj Patoliya</span>, a Software Engineer based in India. I am an Information Technology undergraduate from Charotar University of Science and Technology. A motivated and hardworking individual with great organisational abilities who can handle various priorities while keeping a positive attitude and flexibility."
+      ],
+      typeSpeed: 3,
+      contentType: 'html'
+    });
+  }, 100);
